1e69c75bda3de0268e53dc14d06e155d8daf4935
C. Davis <cd.rattan@gmail.com>
2013-04-25 20:41:43 -0700

    fix copyright, add TODO

--- src/pool.h
+++ src/pool.h
@@ -1,6 +1,8 @@
 /* 
  * This file is part of keeper.
  * 
+ * Copyright 2013, cdavis
+ *  
  * keeper is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
